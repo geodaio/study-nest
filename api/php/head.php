b82fb954c1da8e2f71ee500e8ec0fb5619b5e7a3
geodaio <148104450+geodaio@users.noreply.github.com>
2023-11-21 09:04:48 -0500

    Create head.php

---
+++ api/php/head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    
+    <title>
+    <?php
+      echo $title;
+    ?>
+    </title>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" id="favicon"/>
+    
+    <link rel="stylesheet" type="text/css" href="css/studynest.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;700&family=PT+Serif&display=swap" rel="stylesheet"> <!--Previous three links are links for Google Fonts-->
+</head>
