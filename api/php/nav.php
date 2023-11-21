a1f984d252e97ce94c300ee644ccda747dc74f83
geodaio <148104450+geodaio@users.noreply.github.com>
2023-11-21 09:09:13 -0500

    Create nav.php

---
+++ api/php/nav.php
@@ -0,0 +1,25 @@
+<header id="header-out">
+            <nav class="flexbox">
+                <div class="header-left">
+                    <a href="index.html"><img src="images/SNLogo.svg" alt="Logo Temp" id="header-logo"></a>
+                  <a href="hub.html" title="Study Hub">Study Hub</a>
+                    <a href="" title="Temp">How it Works</a>
+                    <a href="" title="Temp">Resources</a>
+                    <a href="" title="Temp">About Us</a>
+                </div>
+                <div class="header-right">
+                    <div class="dropdown">
+                        <button class="dropdown-button" id="lang"><img src="images/Globe_icon.svg" alt="ALTTEXT" id="dropdown-img">English</button>
+                        <div class="lang-options">
+                            <a href="" title="English" id="English" onclick="changeLang(this.id)">English</a>
+                            <a href="" title="Spanish" id="Spanish" onclick="changeLang(this.id)">Spanish</a>
+                            <a href="" title="French" id="French" onclick="changeLang(this.id)">French</a>
+                            <a href="" title="German" id="German" onclick="changeLang(this.id)">German</a>
+                            <a href="" title="Russian" id="Russian" onclick="changeLang(this.id)">Russian</a>
+                        </div>
+                    </div>
+                    <button class="sign-in" id="sign-in" onclick="toLogin()">Sign In</button>
+                      <button class="sign-up" onclick="">Sign Up</button>
+                </div>
+            </nav>
+        </header>
