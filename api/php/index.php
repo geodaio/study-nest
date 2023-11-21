11eb905b07f02ff2e6cd01f9b0c40bb4fa804c22
geodaio <148104450+geodaio@users.noreply.github.com>
2023-11-21 09:59:57 -0500

    Update index.php
    -Spacing

--- api/php/index.php
+++ api/php/index.php
@@ -14,156 +14,137 @@
             </section>
             <section class="intro-sect-right">
                 <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" class="svg-close">
-               
                     <g id="computer-svg-2"></g>
-                      <rect x="22%" y="20%" width="60%" height="28%" rx="20" id="rect-home-2"></rect>
-                      <clipPath id="clip1">
+                    <rect x="22%" y="20%" width="60%" height="28%" rx="20" id="rect-home-2"></rect>
+                    <clipPath id="clip1">
                         <use xlink:href="#rect-home-2"/>
-                      </clipPath>
-                      <circle cx="65%" cy="22.8%" r="6" id="circle-home-2-1"></circle>
-                      <circle cx="70%" cy="22.8%" r="6" id="circle-home-2-2"></circle>
-                      <circle cx="75%" cy="22.8%" r="6" id="circle-home-2-3"></circle>
-                      <line x1="28%" y1="24.8%" x2="76%" y2="24.8%" id="line-home-2"></line>
-                      <image href="" x="22%" y="20%" width="50%" height="18%" id="img-home-2" clip-path="url(#clip1)"/>
+                    </clipPath>
+                    <circle cx="65%" cy="22.8%" r="6" id="circle-home-2-1"></circle>
+                    <circle cx="70%" cy="22.8%" r="6" id="circle-home-2-2"></circle>
+                    <circle cx="75%" cy="22.8%" r="6" id="circle-home-2-3"></circle>
+                    <line x1="28%" y1="24.8%" x2="76%" y2="24.8%" id="line-home-2"></line>
+                    <image href="" x="22%" y="20%" width="50%" height="18%" id="img-home-2" clip-path="url(#clip1)"/>
                     <use xlink:href="#img-home-2" /> 
                     <g id="computer-svg-1"></g>
-                      <rect x="22%" y="20%" width="60%" height="28%" rx="20" id="rect-home-1"></rect>
-                      <clipPath id="clip2">
+                    <rect x="22%" y="20%" width="60%" height="28%" rx="20" id="rect-home-1"></rect>
+                    <clipPath id="clip2">
                         <use xlink:href="#rect-home-1"/>
-                      </clipPath>
-                      <circle cx="25%" cy="42%" r="6" id="circle-home-1-1"></circle>
-                      <circle cx="30%" cy="42%" r="6" id="circle-home-1-2"></circle>
-                      <circle cx="35%" cy="42%" r="6" id="circle-home-1-3"></circle>
-                      <line x1="28%" y1="44%" x2="76%" y2="44%" id="line-home-1"></line>
-                      <image href="" x="22%" y="20%" width="50%" height="18%" id="img-home-1" clip-path="url(#clip2)"/>
-                 
-                <use xlink:href="#img-home-1" /> 
+                    </clipPath>
+                    <circle cx="25%" cy="42%" r="6" id="circle-home-1-1"></circle>
+                    <circle cx="30%" cy="42%" r="6" id="circle-home-1-2"></circle>
+                    <circle cx="35%" cy="42%" r="6" id="circle-home-1-3"></circle>
+                    <line x1="28%" y1="44%" x2="76%" y2="44%" id="line-home-1"></line>
+                    <image href="" x="22%" y="20%" width="50%" height="18%" id="img-home-1" clip-path="url(#clip2)"/>
+                    <use xlink:href="#img-home-1" /> 
                 </svg>
-            </section>
-        </div>
-        <section id="quote">
-                <!--KAT CODE-->
-                <div class="right">
-                 <div class="row scrolling_text">
-                        <div class="text">
-                        <span>running text</span>
-                        <span>running text</span>
-                        <span>running text</span>
-                        <span>running text</span>
-                        <span>running text</span>
-                        </div>
-                        <div class="text">
-                        <span>running text</span>
-                        <span>running text</span>
-                        <span>running text</span>
-                        <span>running text</span>
-                        <span>running text</span>
-                        </div>
-                    </div>
-                    </div>
-                <!--END KAT CODE-->
-            <div class="flexbox quote2">
-                <h2 id="home-quote">"A place where you and others can start on a studying journey"</h2>
-            </div>
-            
         </section>
-        
-             <div class="flexbox Aboutus">
-            <section class="detail-sect-left">
-                 <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" class="svg-close">
-                    <g id="computer-svg-3"></g>
-                      <rect x="18%" y="20%" width="70%" height="58%" rx="20" id="rect-home-3"></rect>
-                      <clipPath id="clip3">
-                        <use xlink:href="#rect-home-3"></use>
-                      </clipPath>
-                      <circle cx="76.5%" cy="24.8%" r="6" id="circle-home-3-1"></circle>
-                      <circle cx="80%" cy="24.8%" r="6" id="circle-home-3-2"></circle>
-                      <circle cx="83.5%" cy="24.8%" r="6" id="circle-home-3-3"></circle>
-                      <line x1="22%" y1="28.8%" x2="84%" y2="28.8%" id="line-home-3"></line>
-                      <image href="" x="22%" y="20%" width="50%" height="18%" id="img-home-3" clip-path="url(#clip3)"></image>
-                  
-                </svg>
-            </section>
-            <section class="detail-sect-right">
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget lectus non neque volutpat congue ac at tortor. Vivamus cursus luctus diam ac sodales. Nullam eget neque enim. Maecenas finibus lectus a neque tincidunt cursus.</p>
-                <div class="action_btn">
-                <button id="learn-more-link">+Learn more</button>
-                <button id="site-rules-link">+StudyNest Rules</button></div>
-            </section>
+    </div>
+    <section id="quote">
+    <!--KAT CODE-->
+        <div class="right">
+            <div class="row scrolling_text">
+                <div class="text">
+                    <span>running text</span>
+                    <span>running text</span>
+                    <span>running text</span>
+                    <span>running text</span>
+                    <span>running text</span>
+                </div>
+                <div class="text">
+                    <span>running text</span>
+                    <span>running text</span>
+                    <span>running text</span>
+                    <span>running text</span>
+                    <span>running text</span>
+                </div>
+            </div>
         </div>
+    <!--END KAT CODE-->
+        <div class="flexbox quote2">
+            <h2 id="home-quote">"A place where you and others can start on a studying journey"</h2>
+        </div>
+    </section>
+    <div class="flexbox Aboutus">
+        <section class="detail-sect-left">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" class="svg-close">
+                <g id="computer-svg-3"></g>
+                <rect x="18%" y="20%" width="70%" height="58%" rx="20" id="rect-home-3"></rect>
+                <clipPath id="clip3">
+                    <use xlink:href="#rect-home-3"></use>
+                </clipPath>
+                <circle cx="76.5%" cy="24.8%" r="6" id="circle-home-3-1"></circle>
+                <circle cx="80%" cy="24.8%" r="6" id="circle-home-3-2"></circle>
+                <circle cx="83.5%" cy="24.8%" r="6" id="circle-home-3-3"></circle>
+                <line x1="22%" y1="28.8%" x2="84%" y2="28.8%" id="line-home-3"></line>
+                <image href="" x="22%" y="20%" width="50%" height="18%" id="img-home-3" clip-path="url(#clip3)"></image>   
+            </svg>
+        </section>
+        <section class="detail-sect-right">
+            <h2>Lorem Ipsum</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget lectus non neque volutpat congue ac at tortor. Vivamus cursus luctus diam ac sodales. Nullam eget neque enim. Maecenas finibus lectus a neque tincidunt cursus.</p>
+            <div class="action_btn">
+                <button id="learn-more-link">+Learn more</button>
+                <button id="site-rules-link">+StudyNest Rules</button>
+            </div>
+        </section>
+    </div>
         <section id="why-strengths">
             <div class="flexbox">
-             <div>
-        <h2 class="centeredtext">Why Study Nest?</h2>
-        <div class="picwrapper">
-          <div class="picturecards">
-            <img class="imgcard" src="https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGZhY2V8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="image one">
-            <p>Lorem ipsum dolor sit amet, inani labores persequeris pri ut, ex sit necessitatibus. Ex sale nobis gubergren est, at prima argumentum, praesent gubergren neglegentur iu at.</p>
-          </div>
-          <div class="picturecards">
-            <img class="imgcard" src="https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGZhY2V8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="image2">
-            <p>Lorem ipsum dolor sit amet, inani labores persequeris pri ut, ex sit necessitatibus. Ex sale nobis gubergren est, at prima argumentum, praesent gubergren neglegentur iu at.</p>
-          </div>
-          <div class="picturecards">
-            <img class="imgcard" src="https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGZhY2V8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="image3">
-            <p>Lorem ipsum dolor sit amet, inani labores persequeris pri ut, ex sit necessitatibus. Ex sale nobis gubergren est, at prima argumentum, praesent gubergren neglegentur iu at.</p>
-          </div>
-
-        </div>
-
-      </div>
-      <div>
-
-      </div>
-      <div>
-
-      </div>
-    </div>
-  </section>
+                 <div>
+                    <h2 class="centeredtext">Why Study Nest?</h2>
+                    <div class="picwrapper">
+                      <div class="picturecards">
+                        <img class="imgcard" src="https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGZhY2V8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="image one">
+                        <p>Lorem ipsum dolor sit amet, inani labores persequeris pri ut, ex sit necessitatibus. Ex sale nobis gubergren est, at prima argumentum, praesent gubergren neglegentur iu at.</p>
+                      </div>
+                      <div class="picturecards">
+                        <img class="imgcard" src="https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGZhY2V8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="image2">
+                        <p>Lorem ipsum dolor sit amet, inani labores persequeris pri ut, ex sit necessitatibus. Ex sale nobis gubergren est, at prima argumentum, praesent gubergren neglegentur iu at.</p>
+                      </div>
+                      <div class="picturecards">
+                        <img class="imgcard" src="https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGZhY2V8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="image3">
+                        <p>Lorem ipsum dolor sit amet, inani labores persequeris pri ut, ex sit necessitatibus. Ex sale nobis gubergren est, at prima argumentum, praesent gubergren neglegentur iu at.</p>
+                      </div>
+                    </div>
+                  </div>
+            </div>
+        </section>
         <section id="testimony">
             <div class="flexbox">
                 <!--CAROSEL GOES HERE-->
-           <div class="slideshow-container">
-
-        <div class="mySlides">
-          <img src="https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGZhY2V8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="Avatar" style="width:90px">
-          <q>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vehicula nisl felis, quis ultricies odio consectetur ut.</q>
-          <div class="author">
-            <p><b>John Keats</b></p>
-            <p> Place of study</p>
-          </div>
-        </div>
-
-        <div class="mySlides">
-          <img src="https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGZhY2V8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="Avatar" style="width:90px">
-          <q> In pharetra velit arcu, eu ultrices ligula gravida sit amet. Integer posuere mi id lacus blandit euismod ut ac odio.</q>
-          <div class="author">
-            <p class="bold"> John Keats</p>
-            <p> Place of study</p>
-          </div>
-        </div>
-
-        <div class="mySlides">
-          <img src="https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGZhY2V8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="Avatar" style="width:90px">
-          <q>ivamus porttitor nulla nec eleifend molestie. Nunc molestie tempor lorem, non blandit purus sagittis ac. </q>
-          <div class="author">
-            <p><b>John Keats</b></p>
-            <p> Place of study</p>
-          </div>
-        </div>
-
-        <a class="prev" onclick="plusSlides(-1)">❮</a>
-        <a class="next" onclick="plusSlides(1)">❯</a>
-
-      </div>
-
-    </div>
+                <div class="slideshow-container">
+                    <div class="mySlides">
+                      <img src="https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGZhY2V8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="Avatar" style="width:90px">
+                      <q>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vehicula nisl felis, quis ultricies odio consectetur ut.</q>
+                      <div class="author">
+                        <p><b>John Keats</b></p>
+                        <p> Place of study</p>
+                      </div>
+                </div>
+                <div class="mySlides">
+                  <img src="https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGZhY2V8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="Avatar" style="width:90px">
+                  <q> In pharetra velit arcu, eu ultrices ligula gravida sit amet. Integer posuere mi id lacus blandit euismod ut ac odio.</q>
+                  <div class="author">
+                    <p class="bold"> John Keats</p>
+                    <p> Place of study</p>
+                  </div>
+                </div>
+                <div class="mySlides">
+                  <img src="https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGZhY2V8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="Avatar" style="width:90px">
+                  <q>ivamus porttitor nulla nec eleifend molestie. Nunc molestie tempor lorem, non blandit purus sagittis ac. </q>
+                  <div class="author">
+                    <p><b>John Keats</b></p>
+                    <p> Place of study</p>
+                  </div>
+                </div>
+                <a class="prev" onclick="plusSlides(-1)">❮</a>
+                <a class="next" onclick="plusSlides(1)">❯</a>
+              </div>
+            </div>
         </section>
-
-           <div class="flexbox news-section">
+        <div class="flexbox news-section">
             <section class="News-sect-left">
-              <div class="Something"></div>   
+                <div class="Something"></div>   
             </section>
             <section class="News-sect-right">
                 <h2>Join Our Newsletter</h2>
@@ -172,9 +153,6 @@
                   <button id="join-newsletter">Join</button></div>
             </section>
         </div>
-
-
-
     <?php
         include("api/php/footer.php");
     ?>
