7d037dc7c5580a3178e442c6f1724b7e61a9afbd
geodaio <148104450+geodaio@users.noreply.github.com>
2023-11-21 10:24:26 -0500

    Update communities.php

--- api/php/communities.php
+++ api/php/communities.php
@@ -1,1 +1,10 @@
-
+<?php
+    $title = "Communities";
+    include("api/php/head.php");
+?>
+    <body class="homepage">
+        <?php
+            include("api/php/nav.php");
+        ?>
+    </body>
+</html>
