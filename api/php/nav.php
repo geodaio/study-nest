a782900a91d6315fe4e795e79b31d96d0864b4b3
geodaio <148104450+geodaio@users.noreply.github.com>
2023-11-21 09:31:28 -0500

    Update nav.php

--- api/php/nav.php
+++ api/php/nav.php
@@ -1,25 +1,33 @@
 <header id="header-out">
             <nav class="flexbox">
-                <div class="header-left">
-                    <a href="index.html"><img src="api/images/SNLogo.svg" alt="Logo Temp" id="header-logo"></a>
-                  <a href="hub.html" title="Study Hub">Study Hub</a>
-                    <a href="" title="Temp">How it Works</a>
-                    <a href="" title="Temp">Resources</a>
-                    <a href="" title="Temp">About Us</a>
-                </div>
+                <?php
+                        include("nav-left.php");
+            ?>
                 <div class="header-right">
-                    <div class="dropdown">
-                        <button class="dropdown-button" id="lang"><img src="api/images/Globe_icon.svg" alt="ALTTEXT" id="dropdown-img">English</button>
-                        <div class="lang-options">
-                            <a href="" title="English" id="English" onclick="changeLang(this.id)">English</a>
-                            <a href="" title="Spanish" id="Spanish" onclick="changeLang(this.id)">Spanish</a>
-                            <a href="" title="French" id="French" onclick="changeLang(this.id)">French</a>
-                            <a href="" title="German" id="German" onclick="changeLang(this.id)">German</a>
-                            <a href="" title="Russian" id="Russian" onclick="changeLang(this.id)">Russian</a>
-                        </div>
-                    </div>
+                    <?php
+                                    include("change-lang-nav.php");
+                        ?>
                     <button class="sign-in" id="sign-in" onclick="toLogin()">Sign In</button>
                     <button class="sign-up" onclick="">Sign Up</button>
                 </div>
             </nav>
         </header>
+
+<header id="header-out">
+        <nav class="flexbox">
+                <?php
+                        include("nav-left.php");
+                ?>
+                <div class="header-right">
+                        <?php
+                                include("change-lang-nav.php");
+                                if (!isset($_COOKIE["loggedIn"])){
+                                        include("nav-in.php");
+                                }
+                                else {
+                                        include("nav-out.php");
+                                }
+                        ?>
+                </div>
+        </nav>
+</header>
