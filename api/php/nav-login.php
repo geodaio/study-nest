e31ff7d42488c7e6373a235e44619ec19a78f286
geodaio <148104450+geodaio@users.noreply.github.com>
2023-11-21 09:22:34 -0500

    Update nav-login.php

--- api/php/nav-login.php
+++ api/php/nav-login.php
@@ -1,19 +1,12 @@
 <header id="header-in">
 	<nav class="flexbox">
 		<?php
-		inlcude("nav-left.php");
+			inlcude("nav-left.php");
 		?>
 		<div class="header-right">
-			<div class="dropdown">
-				<button class="dropdown-button" id="lang"><img src="api/images/Globe_icon.svg" alt="ALTTEXT" id="dropdown-img">English</button>
-				<div class="lang-options">
-					<a href="" title="English" id="English" onclick="changeLang(this.id)">English</a>
-					<a href="" title="Spanish" id="Spanish" onclick="changeLang(this.id)">Spanish</a>
-					<a href="" title="French" id="French" onclick="changeLang(this.id)">French</a>
-					<a href="" title="German" id="German" onclick="changeLang(this.id)">German</a>
-					<a href="" title="Russian" id="Russian" onclick="changeLang(this.id)">Russian</a>
-				</div>
-			</div>
+			<?php
+				include("change-lang-nav.php");
+			?>
 			<button class="notification-bell" id="notifications-header"><img src="api/images/notification_logo.svg" alt="ALTTEXT" id="notification-bell-img"></button>
 			<div class="dropdown">
 				<button class="dropdown-button" id="profile-header"><img src="/apiimages/profile.jpeg" alt="ALTTEXT" id="profile-pic-header"></button>
