b693a3e990d0da77ed5511312402624909ba78d4
geodaio <148104450+geodaio@users.noreply.github.com>
2023-11-21 10:01:52 -0500

    Update footer.php

--- api/php/footer.php
+++ api/php/footer.php
@@ -25,6 +25,3 @@
 	  	<div class="link-boxes"></div>
 	</div>
 </footer>
-<script type="text/javascript" src="home.js"></script>
-<script type="text/javascript" src="favicon.js"></script>
-<script type="text/javascript" src="logInOut.js"></script>
