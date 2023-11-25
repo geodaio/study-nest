c1d58b9bc2d73efc795e42c3ae7be66ec289bee4
geodaio <148104450+geodaio@users.noreply.github.com>
2023-11-24 23:45:20 -0500

    Create connect.php

---
+++ api/php/connect.php
@@ -0,0 +1,8 @@
+<?php
+
+$servername = "localhost"; // Fill this with DB Host (localhost)
+$username = ""; // FIll this with your DB User
+$password = ""; // Fill this with password created during database setup
+$db = ""; // Fill this with DB Name
+
+?>
