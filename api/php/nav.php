19eb79f8f7aff9cfdeee6f421b876d0fd29aa71a
geodaio <148104450+geodaio@users.noreply.github.com>
2023-11-27 23:33:41 -0500

    Update nav.php

--- api/php/nav.php
+++ api/php/nav.php
@@ -6,7 +6,7 @@
                 <div class="header-right">
                         <?php
                                 include("$_SERVER[DOCUMENT_ROOT]/api/php/change-lang-nav.php");
-                                if ($_COOKIE["loggedIn"] === true){
+                                if (!isset($_COOKIE["loggedIn"]) && $_COOKIE["loggedIn"] === true){
                                         include("$_SERVER[DOCUMENT_ROOT]/api/php/nav-in.php");
                                 }
                                 else {
