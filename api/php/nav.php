26a1c85d7242f54ee25f3cb670a7423758117181
geodaio <148104450+geodaio@users.noreply.github.com>
2023-11-28 01:14:29 -0500

    Update nav.php

--- api/php/nav.php
+++ api/php/nav.php
@@ -1,16 +1,16 @@
 <header id="header-out">
         <nav class="flexbox">
                 <?php
-                        include("$_SERVER[DOCUMENT_ROOT]/api/php/nav-left.php");
+                        include($_SERVER["DOCUMENT_ROOT"] . "/api/php/nav-left.php");
                 ?>
                 <div class="header-right">
                         <?php
-                                include("$_SERVER[DOCUMENT_ROOT]/api/php/change-lang-nav.php");
+                                include($_SERVER["DOCUMENT_ROOT"] . "/api/php/change-lang-nav.php");
                                 if (!isset($_COOKIE["loggedIn"]) && $_COOKIE["loggedIn"] === true){
-                                        include($_SERVER[DOCUMENT_ROOT]."/api/php/nav-in.php");
+                                        include($_SERVER["DOCUMENT_ROOT"] . "/api/php/nav-in.php");
                                 }
                                 else {
-                                        include($_SERVER[DOCUMENT_ROOT]."/api/php/nav-out.php");
+                                        include($_SERVER["DOCUMENT_ROOT"] . "/api/php/nav-out.php");
                                 }
                         ?>
                 </div>
