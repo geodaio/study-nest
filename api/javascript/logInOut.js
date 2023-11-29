c67a5bfcd3227e4e35f792211672f918a364ca24
geodaio <148104450+geodaio@users.noreply.github.com>
2023-11-29 10:29:03 -0500

    Update logInOut.js

--- api/javascript/logInOut.js
+++ api/javascript/logInOut.js
@@ -20,9 +20,31 @@ function toLogin() {
 	window.location.assign("https://study-nest.vercel.app/api/php/login.php");
 }
 window.onload = function () {
+	var found = false;
 	storeCookies("loggedIn", "false");
+ 	console.log("test");
+ 	var allCookies = document.cookie;
+ 	
+ 	var splitCookies = allCookies.split(";");
+ 	
+ 	for (var c = 0; c<=splitCookies.length; c++){
+ 		var cleanCookie;
+ 	    if (c != splitCookies.length){
+ 	      cleanCookie = splitCookies[c].trim();
+ 		}
+ 		var cleanerCookie = cleanCookie.split("=");
+ 		console.log(cleanerCookie);
+ 		if (cleanerCookie[c] === "true") {
+ 	        	window.location.assign("https://geodaio.github.io/interactive-design-final/index-login");
+ 			console.log("loggedin2");
+			found = true;
+ 		}
+	 }
+	if (found == false){
+		storeCookies("loggedIn", "false");
+	}
 	var documentVal = window.location.href;
     	var searchDocument = new URLSearchParams(documentVal);
     
     	searchDocument.append('Content-Type', 'text/css');
-}
+};
