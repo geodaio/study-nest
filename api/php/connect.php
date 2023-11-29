8981b0e8c5dbba12786305f3f37c0f845a3fc2db
geodaio <powheart26@gmail.com>
2023-11-29 18:14:27 -0500

    Delete api/php/connect.php

--- api/php/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$servername = "study-nest-6298.g8z.cockroachlabs.cloud"; // Fill this with DB Host 
-$username = "geodaio"; // FIll this with your DB User
-$password = "x3CLHgV0BauSUdhr0jFB6g"; // Fill this with password created during database setup
-$db = "defaultdb"; // Fill this with DB Name
-$port = "26257";
-
-?>
